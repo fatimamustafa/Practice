9dc95b272d62e9039c4c6010a0fd7679adbef0be
fatimamustafa <64366840+fatimamustafa@users.noreply.github.com>
2020-06-07 20:23:01 +0500

    Add files via upload

---
+++ Practice JS/app.js
@@ -0,0 +1,19 @@
+//alert(144);
+
+//var question = "What is your name?";
+//var defaultAnswer = "human";
+//var spec = prompt(question, defaultAnswer);
+
+var x = prompt("Where does the Pope live?");
+ if (x === "Vatican") {
+ alert("Correct!");
+ }
+
+ var name = prompt("What is my name?")
+ if (name === "Fatima") {
+ alert("Correct");}
+
+ if ( name != "Fatima") {
+     alert("Wrong Answer")
+ }
+
